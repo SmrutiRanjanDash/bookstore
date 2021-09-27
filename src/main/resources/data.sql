6468e610807e6d1782de65b43e21fc3ae1ec63d3
SmrutiRanjanDash <32437595+SmrutiRanjanDash@users.noreply.github.com>
2021-09-27 17:36:32 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,3 @@
-INSERT INTO 
-	TBL_EMPLOYEES (first_name, last_name, email) 
-VALUES
-  	('Lokesh', 'Gupta', 'howtodoinjava@gmail.com'),
-  	('John', 'Doe', 'xyz@email.com');
-
 INSERT INTO
     TBL_BOOKS (name, description, author, type, price, isbn)
 VALUES
